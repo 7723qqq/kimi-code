2fca103ab96d412bd87e76478880fe8309655e00
liruifengv <liruifeng1024@gmail.com>
2026-07-10 21:14:36 +0800

    chore: remove vendored web dist snapshot

--- apps/kimi-code/dist-web/assets/swimlanesDiagram-G3AALYLV-CjWkrGVT.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-Dpof9dbB.js";import{_ as a}from"./mermaid.core-DmyVY7Kd.js";import"./chunk-5VM5RSS4-Bbe3L8A3.js";import"./chunk-XXDRQBXY-Bhg1tyq6.js";import"./chunk-VR4S4FIN-D2lRCaNp.js";import"./chunk-32BRIVSS-BtzNhyZo.js";import"./channel-CqpSxxVG.js";import"./index-BOQZ4885.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
